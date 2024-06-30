c76e3ce7c6295f0fc8b0ed192e960c736759d7a9
P Pavan Kumar Reddy <106873030+Pavan-Kumar235@users.noreply.github.com>
2024-06-30 11:18:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImage1 = "dice" + randomNumber1 + ".png";
+var randomImage2 = "dice" + randomNumber2 + ".png";
 
 document.querySelector(".img1").setAttribute("src",randomImage1);
 document.querySelector(".img2").setAttribute("src",randomImage2);
