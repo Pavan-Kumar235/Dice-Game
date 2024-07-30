bd67f1a1b48f8ad4e6ca0ca61d4dfef50786478f
P Pavan Kumar Reddy <106873030+Pavan-Kumar235@users.noreply.github.com>
2024-07-30 16:05:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ function play() {
     });
 }
 
+
+/*
+document.querySelector(".img1").setAttribute("src",randomImage1);
+document.querySelector(".img2").setAttribute("src",randomImage2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector(".change-text").innerHTML = "Player 1 Wins...";
+} else if(randomNumber1 === randomNumber2) {
+    document.querySelector(".change-text").innerHTML = "Draw...";
+} else {
+    document.querySelector(".change-text").innerHTML = "Player 2 Wins...";
+}
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
 /*
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
